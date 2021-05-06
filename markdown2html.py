7f1f14cbec1f9409cf1ddecc749da7c7d7d325f5
houba <khaldi505>
2021-05-06 21:28:00 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -9,15 +9,23 @@ from os import path as path
 from sys import argv as argv
 
 if __name__ == "__main__":
+
     if len(argv) != 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
-File_in = argv[1]
-File_out = argv[2]
+    File_in = argv[1]
+    File_out = argv[2]
 
-if not path.exists(File_in):
-    sys.stderr.write("Missing {}\n".format(File_in))
-    exit(1)
+    if not path.exists(File_in):
+        sys.stderr.write("Missing {}\n".format(File_in))
+        exit(1)
 
-exit(0)
+    else:
+        f_i = open(File_in, "r")
+        content = f_i.read()
+        f_i.close()
+        f_o = open(File_out, "w")
+        f_o.write(content)
+        f_o.close()
+        exit(0)
