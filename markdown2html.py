5443fa34e0f4bc30e58986e3c706c4142e51f5cf
houba <khaldi505>
2021-05-06 20:09:30 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -3,20 +3,23 @@
 a script that will take a markdown file as input, read from
 the file and create another file with the same content of the first markdown file
 """
-from sys import argv as argv
-from sys import exit as exit
-len_arguments = len(argv)
-if len_arguments < 2:
+from sys import argv
+
+if len(argv) <= 2:
     print("Usage: ./markdown2html.py README.md README.html")
     exit(1)
-output_file = argv[2]
-markdown_file = argv[1]
+
+
+File_name = argv[1]
+Out_File = argv[2]
+
 try:
-    f = open(markdown_file, "r")
-except FileNotFoundError as error:
-    print("Missing " + markdown_file)
-    exit(1)
-else:
+    f = open(File_name, 'r')
+except FileNotFoundError:
+    print("Missing " + File_name)
+    exit (1)
+else: 
     content = f.read()
-    f = open(output_file, "w")
+    f = open(Out_File, 'w')
     f.write(content)
+    exit(0)
