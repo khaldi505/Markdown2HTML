3d189e78b7e150874187040ee78b0241e72ccf7f
houba <khaldi505>
2021-05-06 20:31:16 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """
-a script markdown2html.py that takes an argument 2 strings:
-First argument is the name of the Markdown file
-Second argument is the output file name
+a script that will take a markdown file as input, read from
+the file and create another file with the same content of the first markdown file
 """
-import sys
-from sys import argv as argv 
-
-if len(argv) <= 2:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+from sys import argv as argv
+from sys import exit as exit
+len_arguments = len(argv)
+if len_arguments < 2:
+    print("Usage: ./markdown2html.py README.md README.html")
     exit(1)
-
-
-File_name = argv[1]
-Out_File = argv[2]
-
+output_file = argv[2]
+markdown_file = argv[1]
 try:
-    f = open(File_name, 'r')
-except FileNotFoundError:
-    print("Missing " + File_name)
+    f = open(markdown_file, "r")
+except FileNotFoundError as error:
+    print("Missing " + markdown_file)
     exit(1)
-
 else:
-    exit(0)
+    content = f.read()
+    f = open(output_file, "w")
+    f.write(content)
