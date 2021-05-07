8c52d07eb1b209e8f37e93c22b3b8b43209b123a
houba <khaldi505>
2021-05-07 15:23:51 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -25,7 +25,25 @@ if __name__ == "__main__":
         f_i = open(File_in, "r")
         content = f_i.read()
         f_i.close()
+        content = content.split("\n")
+        counter = 0
+        line = ""
+        result = ""
+        while counter < len(content):
+            if str("#") in content[counter]:
+                Heading_weight = content[counter].count('#')
+                content[counter] = content[counter].split("#")
+                if counter < len(content) - 1:
+                    result += "<h{}>".format(Heading_weight) + \
+                        content[counter][Heading_weight] + \
+                        "</h{}>".format(Heading_weight) + "\n"
+                else:
+                    result += "<h{}>".format(Heading_weight) + \
+                        content[counter][Heading_weight] + \
+                        "</h{}>".format(Heading_weight)
+
+            counter += 1
         f_o = open(File_out, "w")
-        f_o.write(content)
+        f_o.write(str(result))
         f_o.close()
         exit(0)
